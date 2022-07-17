88746d43bf482d85b22ab07e51f1b20c728f5c3d
bverry1208 <109407042+bverry1208@users.noreply.github.com>
2022-07-17 12:00:50 +0900

    2022.07.17

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <HeaderSection />
+    <SliderSection />
+    <ImageSection skill="section" />
+    <ImgTextSection skill="section" />
+</template>
+
+
+<script>
+import HeaderSection from "./components/HeaderSection";
+import SliderSection from "./components/SliderSection";
+import ImageSection from "./components/ImageSection";
+import ImgTextSection from "./components/ImgTextSection";
+
+export default {
+    name: 'App',
+    components: {
+        HeaderSection,
+        SliderSection,
+        ImageSection,
+        ImgTextSection,
+    }
+}
+
+
+</script>
+
+
+<style>
+@import "./App.css";
+</style>
---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,84 @@
+<template>
+        <header id="headerType" class="">
+            <div class="header__inner">
+                <h1 class="header__logo">
+                    <a href="/">
+                    web <em>site</em>
+                    </a>
+                </h1>
+                <nav class="header__menu">
+                    <h2 class="ir_so">메인 메뉴</h2>
+                        <ul>
+                            <li>
+                                <a href="/">header</a>
+                            </li>
+                            <li>
+                                <a href="/">slide</a>
+                            </li>
+                            <li>
+                                <a href="/">images</a>
+                            </li>
+                            <li>
+                                <a href="/">content</a>
+                            </li>
+                            <li>
+                                <a href="/">footer</a>
+                            </li>
+                        </ul>
+                </nav>
+            <div class="header__member">
+                <a href="/">login</a>
+            </div>
+        </div>
+    </header>   
+</template>
+
+<style scoped>
+.header__inner {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #ccc;
+    padding: 0 20px;
+}
+.header__logo {
+    flex-basis: 20%;
+}
+.header__logo a {
+    font-size: 30px;
+    display: inline-block;
+    padding: 5px;
+}
+.header__logo em {
+    font-size: 18px;
+    font-weight: 300;
+    text-transform: uppercase;
+}
+.header__menu {
+    flex-basis: 60%;
+    text-align: center;
+}
+.header__menu ul {
+    padding: 25px;
+}
+.header__menu li {
+    display: inline;
+}
+.header__menu li a {
+    padding: 15px 30px;
+    transition: background 0.3s;
+}
+.header__menu li a:hover {
+    background: #f1f1f1;
+    border-radius: 5px;
+}
+.header__member {
+    flex-basis: 20%;
+    text-align: right;
+}
+.header__member a {
+    border-radius: 50px;
+    border: 1px solid #000;
+    padding: 9px 24px 8px;
+}
+</style>
